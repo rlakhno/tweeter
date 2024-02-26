9c3ad966437bb8b9f66b79625f3c843efba8a444
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-02-26 18:20:49 -0500

    Initial commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+// composer-char-counter.js
+
+$(document).ready(function() {
+  // Event handler for input in the textarea
+  $('.new-tweet textarea').on('input', function() {
+    // Determine the length of the input value
+    var inputLength = $(this).val().length;
+    
+    // Update the counter on the page
+    $(this).closest('.new-tweet').find('.counter').text(140 - inputLength);
+    
+    // Adjust counter color based on character limit
+    var counter = $(this).closest('.new-tweet').find('.counter');
+    if (inputLength > 140) {
+      counter.addClass('over-limit');
+    } else {
+      counter.removeClass('over-limit');
+    }
+  });
+});
